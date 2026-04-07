ffd66a6b0b28587af7688bec14f3b7f28df40799
mslthn <msulthon1909@gmail.com>
2026-04-07 22:22:04 +0700

    feat: create database migration for init db

---
+++ backend/migrations/000001_init_db.up.sql
@@ -0,0 +1,19 @@
+create table if not exists "users" (
+    "id" serial primary key,
+    "email" varchar(255) unique not null,
+    "fullname" varchar(255),
+    "description" varchar(255),
+    "password_hash" varchar(255) not null,
+    "created_at" timestamp default now()
+)
+
+create table if not exists "links" (
+    "id" serial primary key,
+    "user_id" integer REFERENCES users(id) on delete CASCADE,
+    "original_url" text not null,
+    "slug" varchar(255) unique not null,
+    "created_at" timestamp default now(),
+    "deleted_at" timestamp,
+    "click_count" integer DEFAULT 0
+
+)

654ac97eb6d082dac307c535069955537a61a6a2
mslthn <msulthon1909@gmail.com>
2026-04-07 15:45:15 +0700

    feat: create footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+    return (
+        <div>
+            <ul className="list-none flex flex-row gap-5 text-gray-400 font-medium">
+                <li>&copy; 2024 SHORTLINK. THEE DIGITAL ARCHITECT</li>
+                <li>PRIVACY POLICY</li>
+                <li>TERMS OF SERVICE</li>
+                <li>API DOCUMENTATION</li>
+                <li>SUPPORT</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Footer

4731e2c3bf51b64ba7b953d515a77c8d08338a7b
mslthn <msulthon1909@gmail.com>
2026-04-08 14:01:58 +0700

    feat: adding navigation to button

--- frontend/src/components/NavigationBar.jsx
+++ frontend/src/components/NavigationBar.jsx
@@ -1,8 +1,11 @@
+import { useNavigate } from "react-router-dom"
 import Button from "./Button"
 
 const NavigationBar = () => {
+    const navigate = useNavigate()
+    
     return (
-        <div className="flex flex-row justify-between bg-white shadow-gray-100 fixed w-full z-10 px-10 py-5">
+        <div className="flex flex-row items-center justify-between bg-white/50 shadow-gray-100 fixed w-full z-10 px-10 py-2">
             <div>
                 <ul className="list-none flex flex-row gap-4">
                     <li className="text-xl font-extrabold">ShortLink</li>
@@ -12,8 +15,14 @@ const NavigationBar = () => {
                 </ul>
             </div>
             <div className="flex flex-row gap-5">
-                <Button variant="white">Login</Button>
-                <Button variant="blue">Sign up Free</Button>
+                <Button 
+                    variant="white"
+                    onClick={() => navigate('/auth/login')}
+                >Login</Button>
+                <Button 
+                    variant="blue"
+                    onClick={() => navigate('/auth/register')}
+                >Sign up Free</Button>
             </div>
         </div>
     )

f134d1e44d52f2b6ca9922de51d3bcf527f07f53
mslthn <msulthon1909@gmail.com>
2026-04-08 14:16:24 +0700

    feat: create hash lib

---
+++ backend/internal/lib/hash.go
@@ -0,0 +1,26 @@
+package lib
+
+import (
+	// "fmt"
+	"github.com/matthewhartstonge/argon2"
+)
+
+func HashPassword(password string) (string, error) {
+	argon := argon2.DefaultConfig()
+
+	encoded, err := argon.HashEncoded([]byte(password))
+	if err != nil {
+		return "", err
+	}
+
+	return string(encoded), nil
+}
+
+func VerifyPassword(password, hash string) (bool, error) {
+	ok, err := argon2.VerifyEncoded([]byte(password), []byte(hash))
+	if err != nil {
+		return false, err
+	}
+
+	return ok, nil
+}

8c1127aab3d6a21b1c839000525c61c0db6088d9
mslthn <msulthon1909@gmail.com>
2026-04-08 14:04:10 +0700

    chore: styling footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div>
-            <ul className="list-none flex flex-row gap-5 text-gray-400 font-medium">
+            <ul className="list-none flex flex-row gap-5 text-gray-400 font-medium justify-center items-center py-5">
                 <li>&copy; 2024 SHORTLINK. THEE DIGITAL ARCHITECT</li>
                 <li>PRIVACY POLICY</li>
                 <li>TERMS OF SERVICE</li>
